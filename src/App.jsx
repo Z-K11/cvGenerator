32b1952a33b6055a43c1060db986a3df8ffcaa2d
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-09-22 17:47:01 +0500

    ui(app) : add and style top header

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './styles/App.css';
+function Header() {
+  return (
+    <header>
+      <div className="leftContentHeader">
+        <h1>Cv Generator</h1>
+      </div>
+      <div className="rightContentHeader">
+        <Link />
+      </div>
+    </header>
+  );
+}
+function Link() {
+  return (
+    <div className="links">
+      <a href="https://www.linkedin.com/in/zk11/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3ByZVxsb67SO6pJKntsYwbIQ%3D%3D">
+        LinkedIn
+      </a>
+      <a href="https://github.com/Z-K11">GitHub</a>
+      <a href="https://www.facebook.com/profile.php?id=100089228738451">
+        Facebook
+      </a>
+    </div>
+  );
+}
+export default function App() {
+  return <Header />;
+}
